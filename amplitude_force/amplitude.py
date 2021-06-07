05ed7054386ae5924836c956c22578f1d1576a0a
suba351 <suba351@gmail.com>
2021-06-08 00:28:20 +0300

    С учётом знака

--- amplitude_force/amplitude.py
+++ amplitude_force/amplitude.py
@@ -28,29 +28,26 @@ vect_minus = np.array(M_nf.inv() * F_nf)
 matr_plus = np.array(M.inv() * C)
 matr_minus = np.array(M_nf.inv() * C_nf)
 
-print()
-print(matr_plus)
-print(vect_plus)
-print()
-print(f0_kappa0_k0)
 u1 = np.load(r"F:\NIR_4th_semestr\U1.npy", allow_pickle=True)
 u2 = np.load(r"F:\NIR_4th_semestr\U2.npy", allow_pickle=True)
 
 u1 = u1[0].subs([(xi1__, xi1), (xi2__, xi2)])
 u2 = u2[0].subs([(xi1__, xi1), (xi2__, xi2)])
 
+print()
+print(u1, u2)
+
 
 def system(y, t):
     etta, d_etta, f1, d_f1, f2, d_f2 = y
     if etta - u1*f1 - u2*f2 - f0_kappa0_k0[2] > 0:
-        dydt = [d_etta, vect_plus[0][0] - (matr_plus[0][0] * etta + matr_plus[0][1] * f1 + matr_plus[0][2] * f2),
+        return [d_etta, vect_plus[0][0] - (matr_plus[0][0] * etta + matr_plus[0][1] * f1 + matr_plus[0][2] * f2),
                 d_f1, vect_plus[1][0] - (matr_plus[1][0] * etta + matr_plus[1][1] * f1 + matr_plus[1][2] * f2),
                 d_f2, vect_plus[2][0] - (matr_plus[2][0] * etta + matr_plus[2][1] * f1 + matr_plus[2][2] * f2)]
     else:
-        dydt = [d_etta, vect_minus[0][0] - (matr_minus[0][0] * etta + matr_minus[0][1] * f1 + matr_minus[0][2] * f2),
+        return [d_etta, vect_minus[0][0] - (matr_minus[0][0] * etta + matr_minus[0][1] * f1 + matr_minus[0][2] * f2),
                 d_f1, vect_minus[1][0] - (matr_minus[1][0] * etta + matr_minus[1][1] * f1 + matr_minus[1][2] * f2),
                 d_f2, vect_minus[2][0] - (matr_minus[2][0] * etta + matr_minus[2][1] * f1 + matr_minus[2][2] * f2)]
-    return dydt
 
 
 def system1(y, t):
@@ -62,7 +59,7 @@ def system1(y, t):
 etta_0, f1_0, f2_0 = 0.1, 0., 0.
 detta_0, df1_0, df2_0 = 0., 0., 0.
 y0 = [etta_0, detta_0, f1_0, df1_0, f2_0, df2_0]
-t = np.linspace(0, 1, 10001)
+t = np.linspace(0, 10, 10001)
 
 
 result = odeint(system, y0, t)
